f32e033b02c675d9f4578577ab00d96f7cb459f8
Maykon Nazareth <104942943+mayconnazareth@users.noreply.github.com>
2025-07-29 11:12:05 -0300

    comentários no código
    Inserindo os comentários no código

--- Projeto/projeto01.rb
+++ Projeto/projeto01.rb
@@ -1,10 +1,15 @@
+#Digite o nome do Usuário
 print "Digite seu nome: "
 nome = gets.chomp
 
+#Digite o sobrenome do usuário
 print "Digite seu sobrenome: "
 sobrenome = gets.chomp
 
+"Digite a idade do usuário
 print "Digite sua idade: "
 idade = gets.chomp.to_i
+
+#mostra na tela, o nome completo do usuário e sua idade
 
 puts "O nome digitatado foi #{nome} #{sobrenome}. O usuário tem #{idade} anos."
