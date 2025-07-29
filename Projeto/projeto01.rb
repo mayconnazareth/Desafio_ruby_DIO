1e0daa4c55ca10bffdef2b2428c55d52e5006d6c
Maykon Nazareth <mayconnazareth@gmail.com>
2025-07-29 11:08:12 -0300

    enviando os arquivos

---
+++ Projeto/projeto01.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+
+puts "O nome digitatado foi #{nome} #{sobrenome}. O usuário tem #{idade} anos."
